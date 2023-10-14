9f8211c511e504b39b4e06fdda574a353e09cc7f
Klkgang <deniscortezsar@gmail.com>
2023-10-13 22:47:14 -0600

    Mas pequeños cambios

--- database/main.py
+++ database/main.py
@@ -1,21 +1,28 @@
 import sqlite3 as sql
-from flask import Flask, request, jsonify
+from flask import Flask
+
+app = Flask(__name__)
+conn = None
 
 try:
-    sofi = sql.connect('database.db')
-    print ("Conexion exitosa a la base de datos!!")
+    # Establecer conexión a la base de datos
+    conn = sql.connect('database.db')
+    print("Conexión exitosa a la base de datos!")
+    cursor = conn.cursor()
 
-except FileNotFoundError:
-    print("No se podido encontrar la base datos")
+    # Crear la tabla usuarios si no existe
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT
+        )
+    ''')
+    conn.commit()
 
-cursor = sofi.cursor
+except sql.Error as e:
+    print("Error al conectar a la base de datos:", e)
 
-mensaje = ("""
-CREATE TABLE usuarios(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT.                                     
-);""")
+finally:
+    if conn:
+        conn.close()
 
-cursor.execute(mensaje)
-sofi.commit()
-sofi.close

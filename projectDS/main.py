8ee6bd0c626113922b8e0e2f8d91d9d0cc2e0adf
Denis Cortez <139710771+klkgang@users.noreply.github.com>
2023-10-27 18:49:38 +0000

    first alpha version 0.2

--- projectDS/main.py
+++ projectDS/main.py
@@ -1,32 +1,34 @@
-from pytube import YouTube # Import the YouTube class from the pytube module
-from flask import Flask, render_template, request, redirect, url_for, flash     # Import the Flask class from the flask module
+from pytube import YouTube
+from flask import Flask, render_template, request, redirect, url_for, flash
 
-app = Flask(__name__) # Create a Flask app
-app.secret_key = 'lsdgvfkuhgfkiiksdgfffozisdiygkkjhvitr87' # This is a secret key used by Flask to encrypt session cookies
+app = Flask(__name__)
+app.secret_key = 'lsdgvfkuhgfkiiksdgfffozisdiygkkjhvitr87'
 
-@app.route('/') # This is the route to which the user will be redirected when they visit the website
-def index(): # This function will be called when the user visits the index page
-    return render_template('index.html') # Render the index.html template
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/download', methods=['GET', 'POST']) # This is the route to which the form in index.html will be submitted
-def download(): # This function will be called when the user submits the form
-    if request.method == 'POST': # If the user submitted the form
-        if 'url' in  request.form: # If the URL field is empty
-            try: # Try to do this
-                url = request.form['url'] # Get URL from form
-                yt = YouTube(url) #`yt` is an object of type `YouTube
-                video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first() # Get the first video with the highest resolution
-                video.download('static') # Download the video
-                return redirect(url_for('index')) # Redirect to the index page
-            except ValueError: # If the URL is invalid
-                flash('Error: Invalid URL') # Show an error message
-                return redirect(url_for('index')) # Redirect to the index page
+@app.route('/download', methods=['GET', 'POST'])
+def download():
+    if request.method == 'POST':
+        if 'url' in  request.form:
+            try:
+                url = request.form['url']
+                yt = YouTube(url)
+                video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+                video.download('static')
+                return redirect(url_for('index'))
+            except ValueError:
+                flash('Error: Invalid URL')
+                return redirect(url_for('index'))
+            except Exception as e:
+                flash(f'Error: {e}')
+                return redirect(url_for('index'))
         else:
-            print('URL field is empty or not found or not valid')
             flash('Error: URL field is empty')
-            return redirect(url_for('index')) # Redirect to the index page
-    else: # If the user tries to access the page directly
-        return redirect(url_for('index')) # Redirect to the index page
+            return redirect(url_for('index'))
+    else:
+        return redirect(url_for('index'))
 
-if __name__ == '__main__': # If the script is run directly
-    app.run(debug=True) # Run the app in debug mode
+if __name__ == '__main__':
+    app.run()
